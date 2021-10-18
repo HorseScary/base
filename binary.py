68dd72887efc93bb44d47cc702cf967adf18a070
HorseScary <horsescary@gmail.com>
2021-10-18 16:37:15 -0400

    added some useless thing

--- binary.py
+++ binary.py
@@ -4,8 +4,16 @@ def binary(var):
     if int(var) == 0: 
         return (0)
 
+    if var%2:
+        return ('penis') 
     
     return final
 
+    
+
 
 print(binary(input('number: ')))
+
+
+
+
